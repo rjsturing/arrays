7ebe8de28ea70cca63ff2615968feb214f786316
Bobby Steckline <rjsturing@gmail.com>
2023-06-08 08:46:34 -0400

    Complete the assignment

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var friends = ["Alex", "Bryan", "Cindy", "David"];
 var numbers = [2, 4, 6, 8];
 var booleans = [true, false, true, false];
+
+// This will call the friend at number 3 position which is the fourth friend David
+console.log(friends[3]);
+
+// This code will add numbers together
+console.log(numbers[0]+numbers[1]);
+
+// this code will add a true boolean to the beginning
+booleans.unshift("true");
+console.log(booleans)
